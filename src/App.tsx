fb2160885ee1e1054e2544951f10bf4dd87739a9
Aniyogakiza <aimable.niyogakiza@drpgroup.com>
2020-04-16 14:54:17 +0200

    Markdown setup
    Took 1 hour 4 minutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { css, jsx } from '@emotion/core'
+
+// Components
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer';
+import useDarkMode from './userDarkMode';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const { theme, toggleTheme } = useDarkMode();
+    const themeStyles = theme === 'light'? {
+        backgroundColor: '#eee',
+        color: '#000'
+    }: {
+        backgroundColor: '#171616',
+        color: '#fff'
+    }
+    return (
+        <div
+            className="App"
+            style={themeStyles}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <Header theme={theme} toggleTheme={toggleTheme}/>
+            <Main theme={theme}/>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
